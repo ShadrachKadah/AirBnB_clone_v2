53a0511a9ea5e43fa0ca463cf9d56504c13ec384
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-04-03 15:17:13 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
-# Sets up webservers for deployment: (Run script on both servers)
+#A bash script that sets up your web servers for the deployment of web_static
 
-WEBSTATIC="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-#run as root and make the two main dir
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-#create index.html and echo text for testing
-echo "This tests index.html on Nginx config" | sudo tee /data/web_static/releases/test/index.html
-#create symbolic link(shortcut file)
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-#Give ownership of the /data/ folder to the ubuntu user
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i "35i $WEBSTATIC" /etc/nginx/sites-available/default
-sudo service nginx start
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+echo "Welcome to AirBnB" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+serve="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
+sed -i "s/^\tserver_name .*;$/&\n$serve/" /etc/nginx/sites-available/default
+service nginx restart
