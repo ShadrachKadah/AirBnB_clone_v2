736883a58cbc44809a39fdfe44af25add1c5ad59
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-04-21 21:16:06 +0000

    9. Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,35 @@
-CTYPE html>
-<HTML lang="en">
-    <HEAD>
-        <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>States</H1>
-        <UL>
-        {% for state in states|sort(attribute='name') %}
-            <LI>{{ state.id }}: <B>{{ state.name }}</B>
-	        <UL>
-	        {% for city in state.cities|sort(attribute='name') %}
-	            <LI>{{ city.id }}: <B>{{ city.name }}</B></LI>
-	        {% endfor %}
-	        </UL>
-	    </LI>
-        {% endfor %}
-        </UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    """ Method to close the session """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_by_id(id):
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
+        if state.id == id:
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
 
